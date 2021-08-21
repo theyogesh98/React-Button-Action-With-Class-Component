6584528efa146bd9cf32b86db67747e1e0d307e3
Yogesh Rajesh  Phutane <88277315+theyogesh98@users.noreply.github.com>
2021-08-21 10:50:30 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+			pHidden : true,
+		}
+	};
+
+
+    render() {
+    	return(
+    		<div id="main">
+				<button id="click" onClick={ ()=>{
+					this.setState({pHidden : false})
+				}}>Show Para</button>
+				{this.state.pHidden ? null : (
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				)}
+				
+    		</div>
+    	);
+    }
+}
+
+
+export default App;
+
